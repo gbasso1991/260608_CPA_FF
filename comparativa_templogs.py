428b5d5d44d2e321d43a480a3e9fad7abd364318
Giuliano <giulianoandresbasso@gmail.com>
2026-06-09 11:51:09 -0300

    Avances

--- comparativa_templogs.py
+++ comparativa_templogs.py
@@ -8,7 +8,7 @@ import os
 import chardet
 import re
 from datetime import datetime
-from clase_resultados import ResultadosESAR
+# from clase_resultados import ResultadosESAR
 #%% Lector Templog
 def lector_templog(path):
     '''
@@ -31,76 +31,113 @@ def lector_templog(path):
 
 #%% 1- 500uL EG 55 FF 45 LN2 RF300-152
 print('-'*50,'\nEG 55 FF 45 LN2 RF300-152','\n')
-temps_500_CPA_control_vapor = glob("1_LN2_to_RF/*.csv",recursive=True)
-temps_500_CPA_control_vapor.sort()
+temps_500_CPA_EG55FF45_1 = glob("1_LN2_to_RF/*.csv",recursive=True)
+temps_500_CPA_EG55FF45_1.sort()
 i=1
-for p in temps_500_CPA_control_vapor:
+for p in temps_500_CPA_EG55FF45_1:
     print('  -',i,p)
     i+=1
     
 fig1,(ax1,ax2) = plt.subplots(2,1,figsize=(9,9),constrained_layout=True)
-
-for p in temps_500_CPA_control_vapor[:-1]:
-    _,time,temp_CH1, _ = lector_templog(p)
-    ax1.plot(time,temp_CH1,label=os.path.basename(p).split('.')[0])
-ax1.set_title('1.1 - EG 55% FF 45% - LN2 --> RF: 300 kHz - 152 dA',loc='left')
+ax1.set_title('1.1 - EG 55% FF 45% - LN2 --> RF',loc='left')
 ax2.set_title('1.2 - EG 55% FF 45% - LN2 --> RT',loc='left')
 
 
-for p in temps_500_CPA_control_vapor[-1:]:
+for p in temps_500_CPA_EG55FF45_1[:-1]:
     _,time,temp_CH1, _ = lector_templog(p)
-    ax2.plot(time,temp_CH1,label=os.path.basename(p).split('.')[0])
-    ax2.plot(time[125:150],temp_CH1[125:150],'.',c='tab:red')
-    ax2.plot(time[300:340],temp_CH1[300:340],'.',c='tab:red')
+    ax1.plot(time,temp_CH1,label=os.path.basename(p).split('.')[0])
+
+_,time_RT,temp_CH1_RT, _ = lector_templog(temps_500_CPA_EG55FF45_1[-1])
+ax1.plot(time_RT,temp_CH1_RT,'C3--',label=os.path.basename(temps_500_CPA_EG55FF45_1[-1]).split('.')[0])
+    
+
+for p in temps_500_CPA_EG55FF45_1[-1:]:
+    _,time,temp_CH1, _ = lector_templog(p)
+    ax2.plot(time,temp_CH1,'C3--',label=os.path.basename(p).split('.')[0])
+    ax2.plot(time[125:150],temp_CH1[125:150],'o',c='tab:purple',alpha=0.5,zorder=-1)
+    ax2.plot(time[300:340],temp_CH1[300:340],'o',c='tab:purple',alpha=0.5,zorder=-1)
 ax2.set_xlim(60,400)
-ax1.set_xlim(0,180)
+ax1.set_xlim(0,160)
 ax2.set_xlabel('t (s)')
+ax1.legend(title='f = 300 kHz   Idc = 152 dA',
+           loc='lower right',
+           shadow=True,frameon=True)
+ax2.legend(loc='lower right',
+           shadow=True,frameon=True)
 
 for a in (ax1,ax2):
     # a.set_xlim(0,)
     # a.axhline(-120,color='k',ls='--',lw=0.5)
     a.grid()
-    a.legend()
     a.set_ylabel('T (°C)')
-#%%
 
+#%% 2 - 500 uL 
+print('-'*50,'\nEG 55 FF 45 LN2 RF300- Idc= [150, 125, 100, 075, 050] dA','\n')
 
-#%%
-#%% 2 - 500 uL CPA control LN2 
-print('-'*50,'\n500 uL CPA control - Enfriamiento en LN2 - Calentamiento en BT','\n')
-temps_500_CPA_control_LN2 = glob("data/*500uL_CPAcontrol_LN2*",recursive=True)
-for p in temps_500_CPA_control_LN2:
-    print('  -',i,p)
-    i+=1
-    
-fig2,ax2 = plt.subplots(figsize=(9,4.5),constrained_layout=True)
-for p in temps_500_CPA_control_LN2:
+temps_500_EG55_FF45_2 = glob("2_EG55_FF45_LN2_to_RF_150_125_100_075_050/*.csv",recursive=True)
+temps_500_EG55_FF45_2.sort()
+for p in temps_500_EG55_FF45_2:
+    print('  -',os.path.basename(p))
+Idc_values = [15.0, 12.5, 10.0, 7.5, 5.0,0]
+H0=[(h*pendiente_HvsI+ordenada_HvsI)/1000 for h in Idc_values]  
+
+fig2,(ax1,ax2) = plt.subplots(2,1,figsize=(9,9),constrained_layout=True)
+ax1.set_title('2.1 - EG 55% FF 45% - LN2 --> RF - Idc = [150, 125, 100] dA',loc='left')
+ax2.set_title('2.2 - EG 55% FF 45% - LN2 --> RF - Idc = [75, 50, 00] dA',loc='left')
+
+for i,p in enumerate(temps_500_EG55_FF45_2[:3]):
     _,time,temp_CH1, _ = lector_templog(p)
-    ax2.plot(time,temp_CH1,label=os.path.basename(p).split('.')[0])
-ax2.set_title('2 - CPA control - LN2 --> BT')
-ax2.set_xlim(0,)
-ax2.grid()
-ax2.legend()
-ax2.set_ylabel('T (°C)')
+    ax1.plot(time,temp_CH1,label=f'{H0[i]:.1f} kA/m')
+
+for i,p in enumerate(temps_500_EG55_FF45_2[3:]):
+    _,time,temp_CH1, _ = lector_templog(p)
+    ax2.plot(time,temp_CH1,label=f'{H0[i+3]:.1f} kA/m' if i!=2 else '0 kA/m')
+
+for a in (ax1,ax2):
+    a.set_xlim(0,175)
+    a.grid()
+    a.legend(title='f = 300 kHz',loc='lower right',shadow=True,frameon=True)
+    a.set_ylabel('T (°C)')
+ax2.set_xlim(80,350)
+
 ax2.set_xlabel('t (s)')
-#%%3  500 uL EG60 Agua40 LN2
-print('-'*50,'\n500 uL 60% Etilenglicol 40% agua - Enfriamiento en LN2 - Calentamiento en BT','\n')
-temps_500_EG60_agua40_LN2 = glob("data/*500uL_EG60_agua40_LN2*",recursive=True)
-temps_500_EG60_agua40_LN2.sort()
-for p in temps_500_EG60_agua40_LN2:
-    print('  -',i,p)
-    i+=1
 
-fig3,ax3 = plt.subplots(figsize=(9,4.5),constrained_layout=True)
-for p in temps_500_EG60_agua40_LN2:
+#%%3  
+print('-'*50,'\nEG 53 FF 47 LN2 RF300- Idc= [150, 125, 100, 075, 050] dA','\n')
+
+temps_500_EG53_FF47 = glob("3_EG53_FF47_LN2_to_RF_150_125_100_075_050/*.csv",recursive=True)
+temps_500_EG53_FF47.sort()
+for p in temps_500_EG53_FF47:
+    print('  -',os.path.basename(p))
+Idc_values = [15.0, 12.5, 10.0, 7.5, 5.0,0]
+H0=[(h*pendiente_HvsI+ordenada_HvsI)/1000 for h in Idc_values]  
+
+fig3,(ax1,ax2) = plt.subplots(2,1,figsize=(9,9),constrained_layout=True)
+ax1.set_title('3.1 - EG 53% FF 47% - LN2 --> RF - Idc = [150, 125, 100] dA',loc='left')
+ax2.set_title('3.2 - EG 53% FF 47% - LN2 --> RF - Idc = [75, 50, 00] dA',loc='left')
+
+for i,p in enumerate(temps_500_EG53_FF47_2[:3]):
     _,time,temp_CH1, _ = lector_templog(p)
-    ax3.plot(time,temp_CH1,label=os.path.basename(p).split('.')[0])
-ax3.set_title('3 - 60% Etilenglicol 40% agua - LN2 --> BT')
-ax3.set_xlim(0,)
-ax3.grid()
-ax3.legend()
-ax3.set_ylabel('T (°C)')
-ax3.set_xlabel('t (s)')     
+    ax1.plot(time,temp_CH1,label=f'{H0[i]:.1f} kA/m')
+
+for i,p in enumerate(temps_500_EG53_FF47_2[3:]):
+    _,time,temp_CH1, _ = lector_templog(p)
+    ax2.plot(time,temp_CH1,label=f'{H0[i+3]:.1f} kA/m' if i!=2 else '0 kA/m')
+
+for a in (ax1,ax2):
+    a.set_xlim(0,175)
+    a.grid()
+    a.legend(title='f = 300 kHz',loc='lower right',shadow=True,frameon=True)
+    a.set_ylabel('T (°C)')
+ax2.set_xlim(80,350)
+
+ax2.set_xlabel('t (s)')
+
+
+
+
+
+
 #%% 4 -  500uL_CPA_FF_LN2
 print('-'*50,'\n500 uL CPA FF - Enfriamiento en LN2 - Calentamiento en RF','\n')
 temps_500_CPA_FF_LN2 = glob("data/*500uL_CPA_FF_LN2*",recursive=True)
